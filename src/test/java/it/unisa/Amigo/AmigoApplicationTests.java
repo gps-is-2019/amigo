de225dc4922b784dbb1a360002ec32fed0fbfb87
hellswipe <a.luppolo@studenti.unisa.it>
2019-12-17 12:36:35 +0100

    Progetto creato

---
+++ src/test/java/it/unisa/Amigo/AmigoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unisa.Amigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
