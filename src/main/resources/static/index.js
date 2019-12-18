f29b3caf8bee530647209986b9bd9d1bbba9c8ba
GitJoedg <g.degiorgio@studenti.unisa.it>
2019-12-18 18:08:35 +0100

    Add index.css, index.js

---
+++ src/main/resources/static/index.js
@@ -0,0 +1,25 @@
+/**
+ * Esegue l'animazione di "fade in" su un elemento
+ * @param element L'elemento che subirà la transizione
+ */
+function fade(element) {
+    var op = 0.1;  // initial opacity
+    element.style.display = 'block';
+    var timer = setInterval(function () {
+        if (op >= 1){
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1;
+    }, 10);
+}
+
+// Variabili
+
+var h1 = document.querySelector('h1');
+var logo = document.querySelector("img");
+
+// Effetti
+fade(h1);
+fade(logo);
