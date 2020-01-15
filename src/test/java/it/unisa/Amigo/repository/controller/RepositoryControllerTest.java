a83ef4d999ec6f213f48d439f7a255092a7def5d
marioinglese <m.inglese6@studenti.unisa.it>
2020-01-15 12:17:08 +0100

    test uploadDocumento

--- src/test/java/it/unisa/Amigo/repository/controller/RepositoryControllerTest.java
+++ src/test/java/it/unisa/Amigo/repository/controller/RepositoryControllerTest.java
@@ -6,8 +6,11 @@ import it.unisa.Amigo.autenticazione.domain.Role;
 import it.unisa.Amigo.autenticazione.domain.User;
 import it.unisa.Amigo.documento.domain.Documento;
 import it.unisa.Amigo.gruppo.domain.Persona;
+import it.unisa.Amigo.gruppo.domain.Supergruppo;
 import it.unisa.Amigo.gruppo.services.GruppoService;
 import it.unisa.Amigo.repository.services.RepositoryService;
+import it.unisa.Amigo.task.controller.TaskForm;
+import it.unisa.Amigo.task.domain.Task;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -17,24 +20,26 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class RepositoryControllerTest {
-
-    @MockBean
-    private GruppoService gruppoService;
 
     @MockBean
     private RepositoryService repositoryService;
@@ -91,7 +96,46 @@ public class RepositoryControllerTest {
                // Arguments.of(user2, 403,"403")
         );
     }
+    @ParameterizedTest
+    @MethodSource("provideUploadDocumentoPost")
+    void uploadDocumento(User user, MockMultipartFile file, boolean flag,String nameModel, String contentModel) throws Exception {
+        UserDetailImpl userDetails = new UserDetailImpl(user);
+        when(repositoryService.addDocumentoInRepository(file.getOriginalFilename(), file.getBytes(), file.getContentType())).thenReturn(flag);
+        List<Documento> documenti = new ArrayList<>();
+        when(repositoryService.searchDocumentInRepository("")).thenReturn(documenti);
 
+        this.mockMvc.perform(multipart("/repository/uploadDocumento").file(file)
+                .with(csrf())
+                .with(user(userDetails)))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("addFlag", flag))
+                .andExpect(model().attribute(nameModel, contentModel))
+                .andExpect(model().attribute("flagPQA", 1))
+                .andExpect(model().attribute("documenti", documenti))
+                .andExpect(view().name("repository/repository"));
+    }
+    private static Stream<Arguments> provideUploadDocumentoPost() {
+        User user = new User("admin", "admin");
+        user.addRole(new Role(Role.PQA_ROLE));
+        MockMultipartFile file =  new MockMultipartFile("file", "test.pdf", "application/pdf",new byte[1]);
+        MockMultipartFile file2 = new MockMultipartFile("file", "test.txt", "application/pdf",new byte[1]);
+        MockMultipartFile file3 = new MockMultipartFile("file", "test.zip", "application/pdf",new byte[1]);
+        MockMultipartFile file4 = new MockMultipartFile("file", "test.rar", "application/pdf",new byte[1]);
+        MockMultipartFile file5 = new MockMultipartFile("file", "test.exe", "application/pdf",new byte[1]);
+        MockMultipartFile file6 = new MockMultipartFile("file", "test.pdf", "application/pdf",new byte[0]);
+        MockMultipartFile file7 = new MockMultipartFile("file", "test.pdf", "application/pdf",new byte[10485761]);
+
+        return Stream.of(
+                Arguments.of(user,file,true,"documentoNome",file.getOriginalFilename()),
+                Arguments.of(user,file2,true,"documentoNome",file2.getOriginalFilename()),
+                Arguments.of(user,file3,true,"documentoNome",file3.getOriginalFilename()),
+                Arguments.of(user,file4,true,"documentoNome",file4.getOriginalFilename()),
+                Arguments.of(user,file5,false,"errorMessage","Formato del file non supportato"),
+                Arguments.of(user,file6,false,"errorMessage","Dimensioni del file non supportate"),
+                Arguments.of(user,file7,false,"errorMessage","Dimensioni del file non supportate")
+
+        );
+    }
    /* @ParameterizedTest
     @MethodSource("provideUploadDocumento")
     public void uploadDocumento(User user, Persona expectedPersona) throws Exception {
